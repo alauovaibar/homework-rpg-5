d78604ca1d7f5467ddecbdbc7f08e9ba30e308d0
alauovaibar <alauovajbar@gmail.com>
2026-03-15 21:18:14 +0500

    HW 5 Commit 6

--- src/com/narxoz/rpg/facade/RewardService.java
+++ src/com/narxoz/rpg/facade/RewardService.java
@@ -3,9 +3,7 @@ package com.narxoz.rpg.facade;
 public class RewardService {
     public String determineReward(AdventureResult battleResult) {
         // TODO: Decide reward rules based on battle outcome.
-        if (battleResult == null) {
-            return "TODO";
-        }
-        return "TODO";
+        if (battleResult.getRounds() < 5) return "Легендарный сундук золота";
+        return "Мешочек с монетами";
     }
 }
